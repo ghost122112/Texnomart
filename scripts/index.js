9f406806ec5f68a398260744318831a03cbfcb6e
ghost122112 <jalol101.ghost@yandex.com>
2022-11-09 12:59:31 +0500

    .

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+function openCity(evt, blockOrder) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("main__wrapper--section-3--content-table--tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("main__wrapper--section-3--content--tab--tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(blockOrder).style.display = "block";
+    evt.currentTarget.className += " active";
+}
