f252caf8b389fbcb1fcc2e58909f0e9cb7e78013
Jaloliddin Zukhriddinov <jalol101.ghost@yandex.com>
2023-01-05 17:32:18 +0500

    .

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -14,8 +14,13 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  
+  if (n < 1) {
+    slideIndex = slides.length
+  }
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
